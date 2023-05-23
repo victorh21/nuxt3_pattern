9250fdf8bee74f098c1c71620b10ad7be73fbec0
fullzer4 <gabrielpelizzaro@gmail.com>
2023-05-23 17:43:45 +0000

    new

---
+++ libs/typescript/__tests__/global.ts
@@ -0,0 +1,11 @@
+import { describe, test } from 'vitest'
+import { setup } from '@nuxt/test-utils'
+
+describe('Teste de components globais', async () => {
+  await setup({
+    // contexto do teste
+  })
+  test('inicial', () => {
+    // teste
+  })
+})
---
+++ package/dist/main.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const main = () => {
+    const questions = [
+        {
+            type: 'input',
+            name: 'folder',
+            message: 'Qual é o nome do seu aplicativo?'
+        },
+        {
+            type: 'list',
+            name: 'lang',
+            message: 'Linguagem do projeto:',
+            choices: ['javascript', 'typescript']
+        },
+        {
+            type: 'checkbox',
+            name: 'test',
+            message: 'Adicionais:',
+            choices: ['javascript', 'typescript']
+        }
+    ];
+    inquirer.prompt(questions).then(answers => {
+        console.log('Respostas:', answers);
+    });
+};
+main();
